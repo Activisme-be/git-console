d7fbe6badb463e8976fca8c30126c613cab008f9
Tim Joosten <Tjoosten3@gmail.com>
2017-02-09 21:42:14 +0100

    Complete github setup command

--- src/commands/ChangeGitCommand.php
+++ src/commands/ChangeGitCommand.php
@@ -2,13 +2,19 @@
 
 namespace ActivismeBe\Console\Commands;
 
+use ActivismeBe\Console\Helpers\EnvConfig;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
+use Symfony\Component\Console\Question\Question;
+use Symfony\Component\Console\Style\SymfonyStyle;
 
 class ChangeGitCommand extends Command
 {
+    use EnvConfig;
+
     /**
      * Configure the command.
      *
@@ -30,6 +36,35 @@ class ChangeGitCommand extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        //
+        $helper   = $this->getHelper('question');
+
+        $styling  = new SymfonyStyle($input, $output);
+        $question = new ConfirmationQuestion('Are u sure u want to continue? [yes/no]:', false);
+
+        $styling->newLine();
+
+        if (! $helper->ask($input, $output, $question)) { // The doesn't want to continue.
+            $msg = '<error>We will not continue with the function</error>';
+            return $output->writeln($msg);
+        }
+
+        $userQuestion = new Question('GitHub user: ');
+
+        $passQuestion = new Question('GitHub pass: ');
+        $passQuestion->setHidden(true)->setHiddenFallback(false);
+
+        // Update variables
+        $update['GITHUB_USER'] = $helper->ask($input, $output, $userQuestion);;
+        $update['GITHUB_PASS'] = $helper->ask($input, $output, $passQuestion);;
+
+        if ($this->changeEnv($update)) { // .env data changed
+            $outputMsg = '<info>The Github setup is saved!</info>';
+        } else {
+            $outputMsg = '<error>We could not save the GitHub setup.</error>';
+        }
+
+        // Env data doesn't change.
+        $styling->newLine();
+        return $output->writeln($outputMsg);
     }
 }
---
+++ src/config/migrations/14866720338722_envoyer-label-tables.sql
@@ -0,0 +1,6 @@
+-- // Envoyer label tables
+-- Migration SQL that makes the change goes here.
+
+-- @UNDO
+-- SQL to undo the change goes here.
+DROP TABLE labels;
