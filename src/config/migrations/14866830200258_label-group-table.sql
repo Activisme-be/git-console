6e47e9e0bb70f50561cf8ed06c2103d74ad81dd3
Tim Joosten <Tjoosten3@gmail.com>
2017-02-10 00:31:49 +0100

    Update

---
+++ src/config/migrations/14866830200258_label-group-table.sql
@@ -0,0 +1,6 @@
+-- // Label group table
+-- Migration SQL that makes the change goes here.
+
+-- @UNDO
+-- SQL to undo the change goes here.
+DROP TABLE groups;
--- src/database/models/Groups.php
+++ src/database/models/Groups.php
@@ -4,7 +4,7 @@ namespace ActivismeBe\Console\Database\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Labels extends Model
+class Groups extends Model
 {
     protected $table    = 'groups';
     protected $fillable = ['group', 'description'];
