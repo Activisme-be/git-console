a1c389366ac37c801347e4a97cbf2dcc0b6d7295
Tjoosten <Tjoosten3@gmail.com>
2017-02-10 02:10:57 +0100

    Bugfix sql file

--- src/config/migrations/14866830200258_label-group-table.sql
+++ src/config/migrations/14866830200258_label-group-table.sql
@@ -1,5 +1,12 @@
 -- // Label group table
 -- Migration SQL that makes the change goes here.
+CREATE TABLE groups (
+  id          INT(6)         UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  name        VARCHAR(255)   NULL DEFAULT NULL,
+  description TEXT,
+  created_at  TIMESTAMP     NULL DEFAULT NULL,
+  updated_at  TIMESTAMP     NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- @UNDO
 -- SQL to undo the change goes here.
